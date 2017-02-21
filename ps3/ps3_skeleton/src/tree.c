9ed297e0c3be0520a645d45f177fa5d46ff2cd87
Neshat <neshat.dmd@gmail.com>
2017-02-21 18:50:59 +0100

    Implement few helping functions

--- ps3/ps3_skeleton/src/tree.c
+++ ps3/ps3_skeleton/src/tree.c
@@ -10,7 +10,7 @@ node_print ( node_t *root, int nesting )
         if ( root->type == IDENTIFIER_DATA ||
              root->type == STRING_DATA ||
              root->type == RELATION ||
-             root->type == EXPRESSION ) 
+             root->type == EXPRESSION )
             printf ( "(%s)", (char *) root->data );
         else if ( root->type == NUMBER_DATA )
             printf ( "(%ld)", *((int64_t *)root->data) );
@@ -35,6 +35,7 @@ node_init (node_t *nd, node_index_t type, void *data, uint64_t n_children, ...)
         .children = (node_t **) malloc ( n_children * sizeof(node_t *) )
     };
     va_start ( child_list, n_children );
+
     for ( uint64_t i=0; i<n_children; i++ )
         nd->children[i] = va_arg ( child_list, node_t * );
     va_end ( child_list );
@@ -52,7 +53,6 @@ node_finalize ( node_t *discard )
     }
 }
 
-
 void
 destroy_subtree ( node_t *discard )
 {
@@ -63,10 +63,58 @@ destroy_subtree ( node_t *discard )
         node_finalize ( discard );
     }
 }
+bool list_detected ( node_t *node) {
 
+  switch (node->type) {
+    case GLOBAL_LIST:
+    case STATEMENT_LIST:
+    case PRINT_LIST:
+    case EXPRESSION_LIST:
+    case VARIABLE_LIST:
+    case ARGUMENT_LIST:
+    case PARAMETER_LIST:
+    case DECLARATION_LIST:
+      printf("nodetype%s\n", node_string[root->type] );
+      return true;
+      break;
+    default:
+      printf("no list: %s\n", node_string[root->type] );
+      return false;
+  }
+}
+
+node_t prone_and_connect_child ( node_t *node, node_t *parent ) {
+
+  nodet_t *child = node->children[0];
+  node_finalize(node);
+  return child;
+}
+
+// Returns true if the node has only 1 child and no meaningful data
+bool is_syntactic ( node_t *node ) {
+  return node->n_children == 1 && !node->data ;
+}
+
+void flatten_list () {
+  return;
+}
+void traverse ( node_t *root ) {
+
+  node_t *parent = root;
+
+}
 
 void
 simplify_tree ( node_t **simplified, node_t *root )
 {
+    for (size_t i = 0; i < root->n_children; i++) {
+      simplify_tree( &root->children[i], root->children[i] );
+    }
+
+    if ( is_syntactic(root) ) {
+        root = prone_and_switch_parents( root, parent )
+    }
+
+
     *simplified = root;
 }
