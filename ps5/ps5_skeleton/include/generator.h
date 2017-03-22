c8616984ae09b7c164d4778b3a6d77086cd9851b
Neshat Naderi <neshat.dmd@gmail.com>
2017-03-22 17:36:14 +0100

    Implement generator for function call
    Allocate memory for function parameters and local variables

--- ps5/ps5_skeleton/include/generator.h
+++ ps5/ps5_skeleton/include/generator.h
@@ -2,3 +2,12 @@
 #define GENERATOR_H
 void generate_program ( void );
 #endif
+
+#define MIN(a,b) (((a)<(b)) ? (a):(b))
+#define ASM1(op, a) puts("\t"#op"\t"#a)
+#define ASM2(op, a, b) puts("\t"#op"\t"#a", "#b)
+#define FASM2(op, a, b) printf("\t"#op"\t$%l64d, "#a"\n", #b);
+
+static const char *record[6] = {
+    "%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9"
+};
